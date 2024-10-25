a2eb862cfabc63b535a5498b6a5c977b57108feb
arief satrio <ariefsatrio098@gmail.com>
2024-10-25 22:08:50 +0700

    feat: change the form of api logic to be a hooks

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,12 +10,13 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useApiRequest } from "@/hooks/use-api";
 import { useToast } from "@/hooks/use-toast";
 import { api } from "@/lib/axios";
-import { LoginResponse } from "@/types/api";
+import { KaryawanResponse, LoginResponse } from "@/types/api";
 import { AxiosError, AxiosResponse } from "axios";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface LoginData {
     email: string;
@@ -25,6 +26,29 @@ interface LoginData {
 export default function Page(): React.ReactElement {
     const router = useRouter();
     const { toast } = useToast();
+
+    const [loginData, setLoginData] = useState<LoginData | null>(null);
+
+    useApiRequest<LoginResponse>({
+        method: "post",
+        url: "/auth/login",
+        data: loginData,
+        disableAutoNavigate: true,
+        fullfiledCallback(res) {
+            if (res.status == 200) {
+                const token = res.data.data.token;
+                localStorage.setItem("auth-token", token);
+                router.push("/karyawan");
+            }
+        },
+        errorCallback(err) {
+            if (err.status === 401)
+                toast({
+                    title: "Uh oh! Seems like your credentials is wrong.",
+                    description: "Try Again with another email and password.",
+                });
+        },
+    });
 
     function loginHandler(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
@@ -39,74 +63,62 @@ export default function Page(): React.ReactElement {
             password: password as string,
         };
 
-        api.post("/auth/login", data)
-            .then((response: AxiosResponse<LoginResponse>) => {
-                if (response.status == 200) {
-                    const token = response.data.data.token;
-                    localStorage.setItem("auth-token", token);
-                    router.push("/karyawan");
-                }
-            })
-            .catch((error: AxiosError) => {
-                if (error.status === 401)
-                    toast({
-                        title: "Uh oh! Seems like your credentials is wrong.",
-                        description:
-                            "Try Again with another email and password.",
-                    });
-            });
+        setLoginData(data);
     }
 
     return (
-        <div className="flex min-h-screen overflow-hidden">
-            <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-                <div className="mx-auto w-full max-w-sm lg:w-96">
-                    <Card className="border-none shadow-none">
-                        <CardHeader>
-                            <CardTitle className="text-3xl font-bold text-center">
-                                Login
-                            </CardTitle>
-                            <CardDescription className="text-center">
-                                Enter your email below to login to your account
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                            <form
-                                onSubmit={loginHandler}
-                                className="grid gap-6"
-                            >
-                                <div className="grid gap-2">
-                                    <Label htmlFor="email">Email</Label>
-                                    <Input
-                                        id="email"
-                                        type="email"
-                                        name="email"
-                                        placeholder="admin@example.com"
-                                        required
-                                    />
-                                </div>
-                                <div className="grid gap-2">
-                                    <div className="flex items-center justify-between">
-                                        <Label htmlFor="password">
-                                            Password
-                                        </Label>
-                                    </div>
-                                    <Input
-                                        id="password"
-                                        type="password"
-                                        name="password"
-                                        required
-                                    />
-                                </div>
-                                <Button type="submit" className="w-full">
+        <main>
+            <div className="flex min-h-screen overflow-hidden">
+                <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+                    <div className="mx-auto w-full max-w-sm lg:w-96">
+                        <Card className="border-none shadow-none">
+                            <CardHeader>
+                                <CardTitle className="text-3xl font-bold text-center">
                                     Login
-                                </Button>
-                            </form>
-                        </CardContent>
-                    </Card>
+                                </CardTitle>
+                                <CardDescription className="text-center">
+                                    Enter your email below to login to your
+                                    account
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <form
+                                    onSubmit={loginHandler}
+                                    className="grid gap-6"
+                                >
+                                    <div className="grid gap-2">
+                                        <Label htmlFor="email">Email</Label>
+                                        <Input
+                                            id="email"
+                                            type="email"
+                                            name="email"
+                                            placeholder="admin@example.com"
+                                            required
+                                        />
+                                    </div>
+                                    <div className="grid gap-2">
+                                        <div className="flex items-center justify-between">
+                                            <Label htmlFor="password">
+                                                Password
+                                            </Label>
+                                        </div>
+                                        <Input
+                                            id="password"
+                                            type="password"
+                                            name="password"
+                                            required
+                                        />
+                                    </div>
+                                    <Button type="submit" className="w-full">
+                                        Login
+                                    </Button>
+                                </form>
+                            </CardContent>
+                        </Card>
+                    </div>
                 </div>
+                <div className="hidden w-0 flex-1 lg:block bg-primary"></div>
             </div>
-            <div className="hidden w-0 flex-1 lg:block bg-primary"></div>
-        </div>
+        </main>
     );
 }
---
+++ hooks/use-api.tsx
@@ -0,0 +1,65 @@
+import { api } from "@/lib/axios";
+import { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+type HTTPRequestMethod = "post" | "put" | "delete" | "get";
+
+interface useApiRequestOptions<T> {
+    method: HTTPRequestMethod;
+    url: string;
+    data?: any;
+    config?: AxiosRequestConfig;
+    fullfiledCallback?: (res: AxiosResponse<T>) => void;
+    errorCallback?: (err: AxiosError) => void;
+    disableAutoNavigate?: boolean;
+}
+
+export function useApiRequest<T>({
+    url,
+    method,
+    data,
+    config,
+    fullfiledCallback,
+    errorCallback,
+    disableAutoNavigate = false,
+}: useApiRequestOptions<T>) {
+    const [responseData, setResponseData] = useState<T>();
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!data && (method === "post" || method === "put")) {
+            return;
+        }
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const response: AxiosResponse<T> = await api.request({
+                    method: method,
+                    url: url,
+                    data: data,
+                    ...config,
+                });
+
+                setResponseData(response.data);
+                if (fullfiledCallback) fullfiledCallback(response);
+            } catch (err) {
+                const error = err as AxiosError;
+                setError(true);
+                if (!disableAutoNavigate)
+                    if (error.response?.status === 401) {
+                        router.push("/login");
+                    }
+
+                if (errorCallback) errorCallback(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [method, url, data]);
+    return { responseData, error, loading };
+}
